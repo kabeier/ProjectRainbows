94b295370dfc46361d35bdfa66150248465a83b2
kabeier <kabeier@gmail.com>
2026-02-26 15:39:57 -0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+
+export default function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
